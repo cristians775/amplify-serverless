35ce2e64ea1112a0e224102bb527b031a071b40b
Franco Abosso <franco.abosso@gm2dev.com>
2021-10-01 11:22:12 -0300

    Prueba

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
-import  { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 Amplify.configure(awsconfig);
 function App() {
+  console.log('que onda pa');
   return (
     <div className="App">
       <header className="App-header">
